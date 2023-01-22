ea8566b00726577f651f0b6f0f2a43c049c9ba01
aeong98 <sy9815@gmail.com>
2023-01-22 16:00:54 +0900

    run.ts > refactoring

--- src/run.ts
+++ src/run.ts
@@ -8,26 +8,29 @@ export const context = github.context;
 
 const CONFIG_FILENAME = 'pr-branch-labeler.yml';
 export async function run() {
+  if (!context || !context.payload.pull_request) {
+    return;
+  }
+
   const repoToken: string = core.getInput('repo-token', { required: true });
+  const octokit = github.getOctokit(repoToken);
 
-  if (context && context.payload.pull_request) {
-    const octokit = github.getOctokit(repoToken);
-    const config = await getConfigFile(octokit, CONFIG_FILENAME, context);
-    if (!config) {
-      throw new Error('get config file failed');
-    }
+  const config = await getConfigFile(octokit, CONFIG_FILENAME, context);
 
-    const headRef = context.payload.pull_request.head.ref;
-    const baseRef = context.payload.pull_request.base.ref;
-    const parsedConfig = parseConfig(config);
-    const labelsToAdd = getMatchedLabels(parsedConfig, headRef, baseRef);
+  if (!config) {
+    throw new Error('get config file failed');
+  }
 
-    if (labelsToAdd.length > 0) {
-      await octokit.rest.issues.addLabels({
-        issue_number: context.payload.pull_request.number,
-        labels: [...labelsToAdd],
-        ...context.repo,
-      });
-    }
+  const headRef = context.payload.pull_request.head.ref;
+  const baseRef = context.payload.pull_request.base.ref;
+  const parsedConfig = parseConfig(config);
+  const labelsToAdd = getMatchedLabels(parsedConfig, headRef, baseRef);
+
+  if (labelsToAdd.length > 0) {
+    await octokit.rest.issues.addLabels({
+      issue_number: context.payload.pull_request.number,
+      labels: labelsToAdd,
+      ...context.repo,
+    });
   }
 }
